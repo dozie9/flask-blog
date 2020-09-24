a7c067d2c98e1ac66145f962dc77980bd5d2aa4c
Dozie <dozie.steve@gmail.com>
2020-09-24 10:33:37 +0100

    Profile Page and Avatars

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "don't touch this"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(basedir, 'media')
